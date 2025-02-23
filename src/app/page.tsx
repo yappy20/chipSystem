7198c62499dc806fb09867ac6cb7079b7b330938
Elisabeth Yap <ednamode2048@gmail.com>
2025-02-22 19:54:55 -0500

    reeee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="centered"> {/* Use centered class */}
       <div className="content">
-        <h1>Welcome to The Demo Chip System</h1>
+        <h1>Welcome to The Chip System</h1>
         {/* <p>This is a Next.js app using the App Router and TypeScript.</p> */}
         <Link href="login">
           <button className="GetStartedbutton">Get Started</button>
