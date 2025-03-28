c575eea768235f5893633484f0e60e3bc5f2c466
Elisabeth Yap <ednamode2048@gmail.com>
2025-03-28 17:24:58 -0400

    ahhhh

--- src/app/admin/vars.ts
+++ src/app/admin/vars.ts
@@ -1,1 +1,2 @@
-export const ADMIN_EMAILS = ['stein.na@northeastern.edu', 'ednamode2048@gmail.com']
+export const ADMIN_EMAILS = ['stein.na@northeastern.edu', 'ednamode2048@gmail.com', 'k.joseph.hack@gmail.com', 'charlesmccannjr00@gmail.com'
+, 'wilkin17@students.rowan.edu']
