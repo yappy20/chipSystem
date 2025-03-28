ddbd1fd80ad89dde5d50facdcbb86d18d9182015
Elisabeth Yap <ednamode2048@gmail.com>
2025-03-28 10:21:47 -0400

    Update changes

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -6,65 +6,44 @@ import { AdjustChips } from "./AdjustChips";
 import "./admin.css";
 import { ADMIN_EMAILS } from "./vars";
 
-// Instantiate PrismaClient once outside the component to avoid multiple instances
-const prisma = new PrismaClient();
 
 export default async function Page() {
-    // Get session data from NextAuth
     const session = await getServerSession(options);
-
-    // If there is no email in the session or the user is not an admin, redirect
-    if (!session?.user?.email) {
-        redirect('/');
-    }
-    if (!ADMIN_EMAILS.includes(session.user.email)) {
-        redirect('/');
-    }
-
-    try {
-        // Check if the user already exists in the database
-        const existingUser = await prisma.user.findUnique({
-            where: { email: session.user.email },
-        });
-
-        // If the user doesn't exist, add them to the database with initial values
-        if (!existingUser) {
-            await prisma.user.create({
-                data: {
-                    email: session.user.email,
-                    chips: 0, // or any default value you want
-                },
-            });
-        }
-
-        // Fetch all users from the database
-        const users = await prisma.user.findMany();
-
-        // Log all users to the console for debugging
-        console.log(users);
-
-        return (
-            <table>
-                <thead>
-                    <tr>
-                        <th>email</th>
-                        <th>chips</th>
-                        <th>adjust</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map(u => (
-                        <tr key={u.email}>
-                            <td>{u.email}</td>
-                            <td>{u.chips}</td>
-                            <td><AdjustChips email={u.email} /></td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        );
-    } catch (error) {
-        console.error("Error accessing database:", error);
-        // Optionally, you could render an error page or return a message to the user
-    }
+    if (!session?.user?.email) redirect('/');
+    if (!ADMIN_EMAILS.includes(session.user.email)) redirect('/');
+    const prisma = new PrismaClient();
+    const users = await prisma.user.findMany();
+    return <table>
+        <thead>
+            <tr>
+                <th>email</th>
+                <th>chips</th>
+                <th>adjust</th>
+            </tr>
+        </thead>
+        <tbody>
+            {users.map(u => <tr key={u.email}>
+                <td>{u.email}</td>
+                <td>{u.chips}</td>
+                <td><AdjustChips email={u.email} /></td>
+            </tr>)}
+        </tbody>
+    </table>
 }
+
+//tada it works
+//but u have to refresh the page cause server side rendering and im lazy
+
+//i refreshed
+//u see it work
+//the url is http://localhost:3000/admin
+
+//it works however now there needs to be away to get to that page for the admin
+//like that?
+
+//yes
+//soooooo
+//sooo
+//now make it look pretty
+//i do and its not that
+//u dont know what perfection looks like
