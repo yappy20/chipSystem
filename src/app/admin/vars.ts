cdc3584cb4cd997e984674ec49076c684be06769
Elisabeth Yap <ednamode2048@gmail.com>
2025-03-29 11:32:43 -0400

    Update admin emails

--- src/app/admin/vars.ts
+++ src/app/admin/vars.ts
@@ -1,2 +1,2 @@
 export const ADMIN_EMAILS = ['stein.na@northeastern.edu', 'ednamode2048@gmail.com', 'k.joseph.hack@gmail.com', 'charlesmccannjr00@gmail.com'
-, 'wilkin17@students.rowan.edu', 'schadc85@students.rowan.edu']
+, 'wilkin17@students.rowan.edu', 'schadc85@students.rowan.edu', 'belloc79@rowan.edu']
